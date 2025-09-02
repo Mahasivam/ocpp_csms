5c1db5472bcc43d5364dea31738ade7ca11ee2ea
Mahasivam <mahasivam.it@gmail.com>
2025-09-02 12:18:25 +0530

    ocpp errors fixed

--- src/main/java/com/csms/dto/ocpp/ClearCacheRequest.java
+++ src/main/java/com/csms/dto/ocpp/ClearCacheRequest.java
@@ -1,12 +1,10 @@
 package com.csms.dto.ocpp;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ClearCacheRequest {
     // ClearCache request has no payload in OCPP 1.6J specification
     // This DTO exists for structural completeness and future extensibility
